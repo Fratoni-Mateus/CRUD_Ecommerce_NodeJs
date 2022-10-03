f54dc67b5e9b37908db93caaa5109dd8155bb979
Mateus Fratoni <Mateus.Fratonisouza@gmail.com>
2022-10-03 15:21:52 -0300

    Adding a readme file to the project.

--- src/controllers/authControllers.js
+++ src/controllers/authControllers.js
@@ -1,41 +1,42 @@
-const userSchema = require('../models/usersSchema');
-const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
-
+const userSchema = require("../models/usersSchema");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 const SECRET = process.env.SECRET;
 
 const login = (req, res) => {
-    try {
-        userSchema.findOne({ email: req.body.email }, (error, user) =>{
-            if(!user) {
-                return res.status(401).send({
-                    message: "User não encontrado",
-                    email: `${req.body.email}`
-                });
-            }
+  try {
+    userSchema.findOne({ email: req.body.email }, (error, user) => {
+      if (!user) {
+        return res.status(401).send({
+          message: "Login não autorizado",
+          email: `${req.body.email}`,
+        });
+      }
 
-            const validPassword = bcrypt.compareSync(req.body.password, user.password);
+      const validPassword = bcrypt.compareSync(
+        req.body.password,
+        user.password
+      );
 
-            if(!validPassword) {
-                return res.status(401).send({
-                    message: "Login não autorizado"
-                })
-            }
+      if (!validPassword) {
+        return res.status(401).send({
+          message: "Login não autorizado",
+        });
+      }
 
-            const token = jwt.sign({ name: user.name }, SECRET)
+      const token = jwt.sign({ name: user.name }, SECRET);
 
-            res.status(200).send({
-                message: "Login autorizado",
-                token
-            })
-        })
-
-    } catch (error) {
-        console.error(error);
-    }
-}
+      res.status(200).send({
+        message: "Login autorizado",
+        token,
+      });
+    });
+  } catch (error) {
+    console.error(error);
+  }
+};
 
 module.exports = {
-    login
-}
+  login,
+};
